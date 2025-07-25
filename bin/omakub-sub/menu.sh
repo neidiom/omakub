b8516072e5601d0479328b1550e62a3f37c28c3a
nedim <h.nedim@gmail.com>
2025-07-25 13:01:40 +0700

    feat: Enhance main menu with styled header and prompt

--- bin/omakub-sub/menu.sh
+++ bin/omakub-sub/menu.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+source $OMAKUB_PATH/bin/omakub-sub/header.sh
+
 if [ $# -eq 0 ]; then
-	SUB=$(gum choose "Theme" "Font" "Update" "Install" "Uninstall" "Manual" "Quit" --height 10 --header "" | tr '[:upper:]' '[:lower:]')
+	SUB=$(gum choose "Theme" "Font" "Update" "Install" "Uninstall" "Manual" "Quit" --height 10 --header "What do you want to do?" --cursor-prefix "> " --header.foreground "240")
 else
 	SUB=$1
 fi
